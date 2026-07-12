bd79312edf759b2a2d0dcc2fa314c045db30904d
ခွန်ဖန်ဒွဲ့ <127810188+KhunPhanDuae@users.noreply.github.com>
2026-07-13 00:08:45 +0700

    Update app.js

--- app.js
+++ app.js
@@ -47,3 +47,48 @@ table.innerHTML+=`
 }
 
 loadData();
+async function saveData(){
+
+const category=document.getElementById("category").value;
+
+const title=document.getElementById("title").value;
+
+const description=document.getElementById("description").value;
+
+const status=document.getElementById("status").value;
+
+await fetch(API_URL,{
+
+method:"POST",
+
+headers:{
+
+"Content-Type":"application/json"
+
+},
+
+body:JSON.stringify({
+
+data:{
+
+Category:category,
+
+Title:title,
+
+Description:description,
+
+Status:status,
+
+Updated:new Date().toLocaleDateString()
+
+}
+
+})
+
+});
+
+alert("Saved Successfully");
+
+loadData();
+
+}
