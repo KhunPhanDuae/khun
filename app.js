7b5a8123b832cc4d71a1fc69e0e64b8c23235188
ခွန်ဖန်ဒွဲ့ <127810188+KhunPhanDuae@users.noreply.github.com>
2026-07-13 00:04:29 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,49 @@
-console.log("Business Intelligence Dashboard Ready");
+const API_URL = "https://sheetdb.io/api/v1/6zk630pgtw0p4";
+
+async function loadData(){
+
+const response = await fetch(API_URL);
+
+const data = await response.json();
+
+const table = document.querySelector("table");
+
+table.innerHTML=`
+
+<tr>
+
+<th>Category</th>
+
+<th>Title</th>
+
+<th>Status</th>
+
+<th>Updated</th>
+
+</tr>
+
+`;
+
+data.forEach(item=>{
+
+table.innerHTML+=`
+
+<tr>
+
+<td>${item.Category}</td>
+
+<td>${item.Title}</td>
+
+<td>${item.Status}</td>
+
+<td>${item.Updated}</td>
+
+</tr>
+
+`;
+
+});
+
+}
+
+loadData();
